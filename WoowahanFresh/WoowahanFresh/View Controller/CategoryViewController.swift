d6aa2c72bdf4e64552f090848a87e0aa3c789c37
이규현 <qbbang@gmail.com>
2018-11-28 12:36:26 +0900

    Update, 카테고리 하드코딩 완료

--- WoowahanFresh/WoowahanFresh/View Controller/CategoryViewController.swift
+++ WoowahanFresh/WoowahanFresh/View Controller/CategoryViewController.swift
@@ -9,26 +9,90 @@
 import UIKit
 
 class CategoryViewController: UIViewController {
-
-  @IBOutlet weak var TableView: UITableView!
   
-  private var items = [ 1,2,3,4,5,6,7,8,9,10 ]
+  @IBOutlet weak var tableView: UITableView!
+  
+  private var items = [String]()
   
   override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    super.viewDidLoad()
+    tableView.delegate = self
+    tableView.dataSource = self
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     
-  @IBAction func btnSideDish(_ sender: Any) {
-    items = [ 1,2,3,4 ]
+    
+    items = [ "전체보기", "무침", "나물", "볶음", "조림", "젓갈・장・소스", "세트"]
   }
   
-
+  // 버튼 이벤트
+  @IBAction func btnSideDish(_ sender: Any) {
+    items = [ "전체보기", "무침", "나물", "볶음", "조림", "젓갈・장・소스", "세트"]
+    tableView.reloadData()
+  }
+  
+  @IBAction func btnMainDish(_ sender: Any) {
+    items = [ "전체보기", "고기반찬", "해산물 반찬", "분식", "튀김", "전・계란반찬", "세트"]
+    tableView.reloadData()
+  }
+  
+  @IBAction func btnSoup(_ sender: Any) {
+    items = [ "국", "찌개", "탕", "전골", "세트"]
+    tableView.reloadData()
+  }
+  
+  @IBAction func btnKidDish(_ sender: Any) {
+    items = [ "전체보기", "메인반찬", "국・찌개", "간식・음료" ]
+    tableView.reloadData()
+  }
+  
+  @IBAction func btnMeat(_ sender: Any) {
+    items = [ "전체보기", "소고기", "돼지고기", "양념구이", "소스&곁들임찬", "채소", "마무리식사"]
+    tableView.reloadData()
+  }
+  
+  @IBAction func btnKimchi(_ sender: Any) {
+    items = [ "전체보기", "김치", "장아찌・피클" ]
+    tableView.reloadData()
+  }
+  
+  @IBAction func btnGlobal(_ sender: Any) {
+    items = [ "전체보기", "양식", "아시아식"]
+    tableView.reloadData()
+  }
+  
+  @IBAction func btnRice(_ sender: Any) {
+    items = [ "전체보기", "밥", "죽", "면"]
+    tableView.reloadData()
+  }
+  
+  @IBAction func btnSalad(_ sender: Any) {
+    items = [ "전체보기", "샐러드", "샌드위치"]
+    tableView.reloadData()
+  }
+  
+  @IBAction func btnSnack(_ sender: Any) {
+    items = [ "전체보기", "베이커리", "과일", "주스・음료", "스무디", "유제품・커피", "기타간식"]
+    tableView.reloadData()
+  }
+  
+  @IBAction func btnRegularDiet(_ sender: Any) {
+    items = [ "전체보기", "1~2인", "3~4인", "아이반찬"]
+    tableView.reloadData()
+  }
+  
+  @IBAction func btnBrand(_ sender: Any) {
+    items = [ "전체보기", "반찬가게", "반찬 장인", "셰프의 요리", "전국맛집", "키즈관"]
+    tableView.reloadData()
+  }
+  
+  
+  // 버튼 이벤트 끝
+  
 }
 
 extension CategoryViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    print(section)
     return items.count
     
   }
@@ -36,6 +100,10 @@ extension CategoryViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
     print(indexPath.row)
+    
+    //    cell.textLabel!.text = "Cell \(indexPath.section), \(indexPath.row)"
+    cell.textLabel!.text = items[indexPath.row]
+    
     return cell
   }
   
