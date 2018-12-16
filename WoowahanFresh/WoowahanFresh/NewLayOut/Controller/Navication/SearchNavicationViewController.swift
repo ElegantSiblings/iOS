96384cb72ec8e84ef49d69d596863896af98e22b
이규현 <qbbang@gmail.com>
2018-12-16 16:13:56 +0900

    error: must pas a class of kind UITableViewCell

--- WoowahanFresh/WoowahanFresh/NewLayOut/Controller/Navication/SearchNavicationViewController.swift
+++ WoowahanFresh/WoowahanFresh/NewLayOut/Controller/Navication/SearchNavicationViewController.swift
@@ -9,22 +9,69 @@
 import UIKit
 
 class SearchNavicationViewController: UIViewController {
-
-    override func viewDidLoad() {
+  
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var searchBar: UISearchBar!
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    navigationItem.title = "상품 검색"
+    tableView.delegate = self
+    tableView.dataSource = self
+    searchBar.delegate = self
+    tableView.register(UITableView.self, forCellReuseIdentifier: "Cell")
     }
-    */
-
 }
+
+extension SearchNavicationViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    return 3
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    
+    cell.textLabel?.text = "123"//"\(indexPath.row)"
+    
+    return cell
+  }
+  
+}
+
+extension SearchNavicationViewController: UITableViewDelegate {
+}
+
+extension SearchNavicationViewController: UISearchBarDelegate {
+}
+/*
+extension SearchNavicationViewController: UISearchResultsUpdating {
+  func updateSearchResults(for searchController: UISearchController) {
+    print("1111")
+  }
+  
+  
+}
+
+extension SearchNavicationViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    
+    return cell
+  }
+  
+  
+}
+
+extension SearchNavicationViewController: UITableViewDelegate {
+  
+}
+*/
